6baafcd855dbe0265f58b7103c3f2fc5f0278bb2
goykamol <benji.goy@outlook.fr>
2023-03-27 12:13:57 +0200

    modified:   Pycode/DB.py

--- Pycode/DB.py
+++ Pycode/DB.py
@@ -17,6 +17,7 @@ class Database():
         self.host = host
         self.port = port
         self.conn = None
+        self.table = self.find_DB_table()
 
     def __repr__(self):
         self.connect()
@@ -48,7 +49,43 @@ class Database():
         except OperationalError as e:
             print(f"The error '{e}' occured")
             self.conn = None
-
+        
+    def find_DB_table(self):
+        self.connect()
+        dict = {}
+        if self.conn:
+            query = """SELECT table_name FROM information_schema.tables
+        WHERE table_schema = 'public'"""
+            with self.conn:
+                with self.conn.cursor() as curs:
+                    try:
+                        curs.execute(query)
+                        record = curs.fetchall()
+                    except OperationalError as e:
+                        print(f"The error '{e}' occured in '__repr__'")
+                        record = None
+            for table in record:
+                columns = self.__find_table_column(table[0])
+                dict[table[0]] = columns
+        self.conn.close()
+        return dict
+    
+    def __find_table_column(self, table_name):
+        columns = []
+        if self.conn:
+            query = """SELECT column_name FROM information_schema.columns
+        WHERE table_name = '{}'""".format(table_name)
+            with self.conn:
+                with self.conn.cursor() as curs:
+                    try:
+                        curs.execute(query)
+                        record = curs.fetchall()
+                    except OperationalError as e:
+                        print(f"The error '{e}' occured in '__repr__'")
+                        record = None
+            for col in record:
+                columns.append(col[0])
+        return columns
 
 class Database_Insert(Database):
     def import_test_data(self, path = "data_pour_test/data.xml"):
@@ -76,8 +113,67 @@ class Database_Insert(Database):
         self.conn.commit()
         self.conn.close()
 
-    def Insert_chantier(self, debut, fin, commentaire, materiau, clientname, facture, idclient = None):
+    def Insert(self, table_name, data):
+        """
+        Ajoute des donnée dans la table demandé
+        INPUT :
+            - table_name : nom de la table 
+            - data : dictionnaire avec pour clefs le nom des colonnes de la table
+                     et en valeur une liste des données à ajouter, il faut que toutes
+                     les listes soit de la même taille
+        OUTPUT None, ajoute dans la table spécifié les données de data.
+        """
+        test = True
+        n = 1
+        if(type(next(iter(data.values()))) == list):
+        #on vérifie si les listes sont de la même taille
+            
+            n = len(next(iter(data.values())))
+            for val in data.values():
+                if len(val) != n : test = False
+        if not test : print("Les données de data ne font pas la même taille, rien n'a été ajouté")
+        else :
+            query = self.query_insert(table_name, data, n)
+            self.connect()
+            if self.conn:
+                with self.conn:
+                    with self.conn.cursor() as curs:
+                        try:
+                            curs.execute(query)
+                        except OperationalError as e:
+                            print(f"The error '{e}' occured in 'Insert_client''")
+            self.conn.commit()
+            self.conn.close()
         return None
+    
+    def query_insert(self, table_name, data, n):
+        """
+        créé la query 'INSERT INTO' à partir d'un nom de table et d'un dictionnaire de données
+        """
+        query = "INSERT INTO {}(".format(table_name)
+        for i, key in enumerate(data.keys()):
+            if i != 0 : query += ", " + key
+            else : query += key
+        query += ") VALUES"
+        liste = []
+        for value in data.values():
+            liste.append(value)
+        print(liste)
+        if n == 1:
+            query += " ('" + str(liste[0]) + "'"
+            for elt in liste[1:]:
+                query += ", '" + elt + "'"
+            query += ");"
+        else:
+                query += " ('" + str(liste[0][0]) + "'"
+                for j in range(1, len(liste)):
+                    query += ", '" + str(liste[j][0]) + "'"
+                for i in range(1,n):
+                    query += "), ('" + str(liste[0][i]) + "'"
+                    for j in range(1, len(liste)):
+                        query += ", '" + str(liste[j][i]) + "'"
+                query += ");"
+        return query
 
 
 class Database_Read(Database):
@@ -109,6 +205,10 @@ a = Database("postgres", "postgres", "admin","localhost","5432")
 
 print(a)
 
+data = {"nom" : ["amazon", "fnac", "mcdo"]}
 
+i = Database_Insert("postgres", "postgres", "admin","localhost","5432")
+
+i.Insert("client", data)
 
 
