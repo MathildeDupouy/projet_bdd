c27dabf5773ffc64cecb7e862265a7329d2dc9ed
goykamol <benji.goy@outlook.fr>
2023-03-31 10:46:28 +0200

    modified:   Pycode/DB.py

--- Pycode/DB.py
+++ Pycode/DB.py
@@ -16,29 +16,26 @@ class Database():
         self.pwd = pwd
         self.host = host
         self.port = port
-        self.conn = None
+        self.connect()
         self.table = self.find_DB_table()
 
     def __repr__(self):
-        self.connect()
         txt = ""
-        if self.conn:
-            query = """SELECT table_name FROM information_schema.tables
+        query = """SELECT table_name FROM information_schema.tables
         WHERE table_schema = 'public'"""
-            with self.conn:
-                with self.conn.cursor() as curs:
-                    try:
-                        curs.execute(query)
-                        record = curs.fetchall()
-                    except OperationalError as e:
-                        print(f"The error '{e}' occured in '__repr__'")
-                        record = None
-            txt +=  "              List of relations\n"
-            txt += "Schema |        Name        | Type  |  Owner\n"
-            txt += "-------+--------------------+-------+----------\n"
-            for name in record:
-                txt += "public | {:^19}| table | {}\n".format(name[0], self.user)
-        self.conn.close()
+        with self.conn:
+            with self.conn.cursor() as curs:
+                try:
+                    curs.execute(query)
+                    record = curs.fetchall()
+                except OperationalError as e:
+                    print(f"The error '{e}' occured in '__repr__'")
+                    record = None
+        txt +=  "              List of relations\n"
+        txt += "Schema |        Name        | Type  |  Owner\n"
+        txt += "-------+--------------------+-------+----------\n"
+        for name in record:
+            txt += "public | {:^19}| table | {}\n".format(name[0], self.user)
         return txt
     
     def connect(self):
@@ -51,40 +48,36 @@ class Database():
             self.conn = None
         
     def find_DB_table(self):
-        self.connect()
         dict = {}
-        if self.conn:
-            query = """SELECT table_name FROM information_schema.tables
+        query = """SELECT table_name FROM information_schema.tables
         WHERE table_schema = 'public'"""
-            with self.conn:
-                with self.conn.cursor() as curs:
-                    try:
-                        curs.execute(query)
-                        record = curs.fetchall()
-                    except OperationalError as e:
-                        print(f"The error '{e}' occured in '__repr__'")
-                        record = None
-            for table in record:
-                columns = self.__find_table_column(table[0])
-                dict[table[0]] = columns
-        self.conn.close()
+        with self.conn:
+            with self.conn.cursor() as curs:
+                try:
+                    curs.execute(query)
+                    record = curs.fetchall()
+                except OperationalError as e:
+                    print(f"The error '{e}' occured in '__repr__'")
+                    record = None
+        for table in record:
+            columns = self.__find_table_column(table[0])
+            dict[table[0]] = columns
         return dict
     
     def __find_table_column(self, table_name):
         columns = []
-        if self.conn:
-            query = """SELECT column_name FROM information_schema.columns
-        WHERE table_name = '{}'""".format(table_name)
-            with self.conn:
-                with self.conn.cursor() as curs:
-                    try:
-                        curs.execute(query)
-                        record = curs.fetchall()
-                    except OperationalError as e:
-                        print(f"The error '{e}' occured in '__repr__'")
-                        record = None
-            for col in record:
-                columns.append(col[0])
+        query = """SELECT column_name FROM information_schema.columns
+    WHERE table_name = '{}'""".format(table_name)
+        with self.conn:
+            with self.conn.cursor() as curs:
+                try:
+                    curs.execute(query)
+                    record = curs.fetchall()
+                except OperationalError as e:
+                    print(f"The error '{e}' occured in '__repr__'")
+                    record = None
+        for col in record:
+            columns.append(col[0])
         return columns
 
 class Database_Insert(Database):
@@ -93,25 +86,22 @@ class Database_Insert(Database):
         return None
 
     def Insert_client(self, noms, show = False):
-        self.connect()
-        if self.conn:
-            if (type(noms) == list):
-                query = """INSERT INTO client(nom)
-                    VALUES ('{}')""".format(noms[0])
-                for nom in noms[1:]:
-                    query += ", ('{}')".format(nom)
-                query+=";"
-            else:
-                query = """INSERT INTO client(nom)
-                    VALUES ('{}');""".format(noms)
-            with self.conn:
-                with self.conn.cursor() as curs:
-                    try:
-                        curs.execute(query)
-                    except OperationalError as e:
-                        print(f"The error '{e}' occured in 'Insert_client''")
+        if (type(noms) == list):
+            query = """INSERT INTO client(nom)
+                VALUES ('{}')""".format(noms[0])
+            for nom in noms[1:]:
+                query += ", ('{}')".format(nom)
+            query+=";"
+        else:
+            query = """INSERT INTO client(nom)
+                VALUES ('{}');""".format(noms)
+        with self.conn:
+            with self.conn.cursor() as curs:
+                try:
+                    curs.execute(query)
+                except OperationalError as e:
+                    print(f"The error '{e}' occured in 'Insert_client''")
         self.conn.commit()
-        self.conn.close()
 
     def Insert(self, table_name, data):
         """
@@ -134,16 +124,13 @@ class Database_Insert(Database):
         if not test : print("Les données de data ne font pas la même taille, rien n'a été ajouté")
         else :
             query = self.query_insert(table_name, data, n)
-            self.connect()
-            if self.conn:
-                with self.conn:
-                    with self.conn.cursor() as curs:
-                        try:
-                            curs.execute(query)
-                        except OperationalError as e:
-                            print(f"The error '{e}' occured in 'Insert_client''")
+            with self.conn:
+                with self.conn.cursor() as curs:
+                    try:
+                        curs.execute(query)
+                    except OperationalError as e:
+                        print(f"The error '{e}' occured in 'Insert_client''")
             self.conn.commit()
-            self.conn.close()
         return None
     
     def query_insert(self, table_name, data, n):
@@ -179,29 +166,45 @@ class Database_Insert(Database):
         """
         insère dans la table les données de filename qui doit être placé dans le dossier data
         """
+        tablename = []
+        les_dict = []
         with open("data/{}".format(filename), 'r') as f:
+            next_flag = True
+            for line in f.readlines():
+                if next_flag:
+                    next_flag = False
+                    linesplit = line.strip().split()
+                    tablename .append(linesplit[0])
+                    dict = {}
+                    col_name = []
+                    for i, col in enumerate(linesplit[1:]):
+                        if col != '':
+                            col_name.append(col)
+                else:
+                    linesplit = line.strip().split()
+                    if linesplit[0] == "next":
+                        next_flag = True
+// - - - - - - - - -  - - - - - - - - à finir
+
             
 class Database_Read(Database):
     def get_current_chantiers(self, show = False):
-        self.connect()
-        if self.conn:
-            query = """SELECT Cl.nom, Ch.debut, ch.fin, ch.commentaire
-            FROM Chantier Ch
-            JOIN CLient Cl ON  Ch.id_client = Cl.id
-            WHERE Ch.debut < NOW() AND Ch.fin > NOW();"""
-            with self.conn:
-                with self.conn.cursor() as curs:
-                    try:
-                        curs.execute(query)
-                        record = curs.fetchall()
-                    except OperationalError as e:
-                        print(f"The error '{e}' occured in 'get_current_chantier'")
-                        record = None
-            if show == True:
-                print(f"Nombre de chantier en cours : {len(record)}")
-                for i, line in enumerate(record):
-                    print(f"Chantier {i} : {line[0]} de {line[1]} à {line[2]}, {line[3]}")
-        self.conn.close()
+        query = """SELECT Cl.nom, Ch.debut, ch.fin, ch.commentaire
+        FROM Chantier Ch
+        JOIN CLient Cl ON  Ch.id_client = Cl.id
+        WHERE Ch.debut < NOW() AND Ch.fin > NOW();"""
+        with self.conn:
+            with self.conn.cursor() as curs:
+                try:
+                    curs.execute(query)
+                    record = curs.fetchall()
+                except OperationalError as e:
+                    print(f"The error '{e}' occured in 'get_current_chantier'")
+                    record = None
+        if show == True:
+            print(f"Nombre de chantier en cours : {len(record)}")
+            for i, line in enumerate(record):
+                print(f"Chantier {i} : {line[0]} de {line[1]} à {line[2]}, {line[3]}")
         return record
 
 
@@ -212,8 +215,6 @@ print(a)
 
 data = {"nom" : ["amazon", "fnac", "mcdo"]}
 
-i = Database_Insert("postgres", "postgres", "admin","localhost","5432")
+i = Database_Insert("projet", "admin", "admin","localhost","5432")
 
 i.Insert("client", data)
-
-
